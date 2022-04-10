3fb82c88b3d82f68c1e33e7bf3423cf289ad8ceb
radhika <rvramayra1998@gmail.com>
2022-04-10 11:17:58 +0530

    nav

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
+  const links = ["service","Project","About"]
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='mid_div'><div>LOGOBAKERY</div>
+    <div className='links'>{links.map((e)=>{
+      return <p>{e}</p>
+    })}
+      </div>
+    <button>Contect</button></div>
     </div>
   );
 }
